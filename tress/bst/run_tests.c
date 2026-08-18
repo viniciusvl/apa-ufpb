0c562c8cd086423dbf2900af0c815ed7f8da0390
viniciusvl <viniciusvictor443@gmail.com>
2026-08-18 19:01:52 -0300

    Remove run_tests.c as it is no longer needed for testing the BST implementation

--- tress/bst/run_tests.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(){
-    int ret = system("cd c:\\Users\\vinic\\Desktop\\FACULDADE\\APA\\apa-ufpb\\tress\\bst && gcc -Wall main.c bst.c -o bst_exec && bst_exec");
-    return ret >> 8;
-}
